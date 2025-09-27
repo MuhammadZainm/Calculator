b1b68cdb6bff19157cd75b68842eab17282a1b2b
MuhammadZainm <mzainmalik71@gmail.com>
2025-09-27 16:20:30 +0500

    day 2

---
+++ script.js
@@ -0,0 +1,44 @@
+//  function add(a,b){
+// let res=a+b
+// alert(res)
+// return res
+
+// }
+// function mul(a,b){
+// let res=a*b
+// alert(res)
+// return res
+
+// }
+// function dif(a,b){
+// let res=a-b
+// alert(res)
+// return res
+
+// }
+// function divi(a,b){
+// let res=a/b
+// alert(res)
+// return res
+
+// }
+// function operate(){
+//     let opr=prompt("Enter operator from+,-,*,/")
+//     let num1=prompt("Enter First Number ")
+//   let  num2=prompt("Enter Second Number ")
+ 
+//   if (opr=="+"){
+//     add(parseInt(num1),parseInt(num2))
+//   }
+//   else if (opr=="-"){
+//     dif(parseInt(num1),parseInt(num2))
+//   }
+//   else if (opr=="*"){
+//     mul(parseInt(num1),parseInt(num2))
+//   }
+//   else if (opr=="/"){
+//     divi(parseInt(num1),parseInt(num2))
+//   }
+    
+// }
+// operate()
